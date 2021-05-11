dc390d780c3a4c5f5d1e1ade99c4a2cec6986610
fahhome <fahmidjobs@gmail.com>
2021-05-11 15:41:53 +0530

    First Commit

---
+++ egit-gateway-server/src/main/java/fahmid/islam/egitgatewayserver/EgitGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package fahmid.islam.egitgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgitGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgitGatewayServerApplication.class, args);
+	}
+
+}
---
+++ egit-gateway-server/src/test/java/fahmid/islam/egitgatewayserver/EgitGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fahmid.islam.egitgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgitGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
